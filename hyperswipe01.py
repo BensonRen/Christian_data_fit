3dfbe9e2524b9b1c11f428d26c4161ba0a045fec
Ben <sr365@duke.edu>
2020-11-11 13:01:36 -0500

    saving the updated

--- hyperswipe01.py
+++ hyperswipe01.py
@@ -4,7 +4,7 @@ This .py file is to run train.py for hyper-parameter swipping in a linear fashio
 import train
 #os.environ["CUDA_VISIBLE_DEVICE"] = "-1"               #Uncomment this line if you want to use CPU only
 import  numpy as np
-import flag_reader
+import flagreader
 if __name__ == '__main__':
     #linear_unit_list = [ 50, 100, 200, 500]
     #linear_unit_list = [1000, 500]
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # reg_scale_list = [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3]
     #reg_scale_list = [5e-4]
     for num_lor in range(4,8):
-        flags = flag_reader.read_flag()  	#setting the base case
+        flags = flagreader.read_flag()  	#setting the base case
         flags.num_lor = num_lor
         flags.spectra_save_dir = '/work/sr365/Christian_data_fit/num_lor' + str(num_lor)
         train.training_from_flag(flags)
